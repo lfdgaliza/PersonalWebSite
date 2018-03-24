7cfdc20ea12088a6f2273dbb06b82049956ec0d3
Luiz Galiza <lfdgaliza@gmail.com>
2018-03-24 13:43:35 +1000

    Adding routes and icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: 'full' },
+  { path: "home", component: HomeComponent },
+  { path: "resume", component: ResumeComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
