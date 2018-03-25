0013f5bbde17c18064fa26eea0ea7ba69dd571f8
Luiz Galiza <lfdgaliza@gmail.com>
2018-03-26 09:22:38 +1000

    highlight color change

--- src/app/-mock/experiences.mock.ts
+++ src/app/-mock/experiences.mock.ts
@@ -11,13 +11,13 @@ export const EXPERIENCES: Experience[] = [
       description: `<p>Using agile methodologies like Scrum and XP, I had the opportunity to participate in three projects with different teams.</p>
     
       <p>The 3rd one was a web application with Angular 2, WCF and other technologies aiming to understand the impact of weather on clean power generation.
-      <br /><span style="color: #DB1E60; font-size: .8em;">Angular 2+ | WCF | SQL Server | .Net 4.6 with C# | Castle Windsor (IoC) | AspNet MVC + WebAPI | Aspose | TFS | Scrum</span></p>
+      <br /><span class="highlight smaller-font">Angular 2+ | WCF | SQL Server | .Net 4.6 with C# | Castle Windsor (IoC) | AspNet MVC + WebAPI | Aspose | TFS | Scrum</span></p>
       
       <p>The 2nd one was a system written in WPF aiming to control load forecasts.
-      <br /><span style="color: #DB1E60; font-size: .8em;">WPF | VSTO | .Net 4.5 with C# | Castle Windsor (IoC) | Informix | TFS | Scrum</span></p>
+      <br /><span class="highlight smaller-font">WPF | VSTO | .Net 4.5 with C# | Castle Windsor (IoC) | Informix | TFS | Scrum</span></p>
       
       <p>And the 1st one was an ASP.NET MVC system aiming to maintain official journal publications and to control the taking actions according to these publications. 
-      <br /><span style="color: #DB1E60; font-size: .8em;">jQuery | AspNet MVC | SQL Server | .Net 4.6 with C# | Castle Windsor (IoC) | TFS | Scrum</span></p>`
+      <br /><span class="highlight smaller-font">jQuery | AspNet MVC | SQL Server | .Net 4.6 with C# | Castle Windsor (IoC) | TFS | Scrum</span></p>`
     },
     {
       title: "Technical Sales Specialist (APM)",
@@ -37,7 +37,7 @@ export const EXPERIENCES: Experience[] = [
       startDate: "May 2013",
       endDate: "Jan 2014",
       description: `<p>Responsible for the entire development of an Amazon trees extraction control system.</p>
-      <p>I chose to use <span style="color: #DB1E60; font-size: .8em;">Knockout</span> as frontend (plus <span style="color: #DB1E60; font-size: .8em;">Bootstrap</span>) and <span style="color: #DB1E60; font-size: .8em;">.Net 4.5 plus C#</span> as backend. The database was <span style="color: #DB1E60; font-size: .8em;">SQL Server.</span></p>`
+      <p>I chose to use <span class="highlight smaller-font">Knockout</span> as frontend (plus <span class="highlight smaller-font">Bootstrap</span>) and <span class="highlight smaller-font">.Net 4.5 plus C#</span> as backend. The database was <span class="highlight smaller-font">SQL Server.</span></p>`
     },
     {
       title: "Developer",
@@ -47,7 +47,7 @@ export const EXPERIENCES: Experience[] = [
       startDate: "Sep 2011",
       endDate: "May 2013",
       description: `I was contracted to develop a Web-based NOC support system.
-      When the first version was delivered - which I wrote in <span style="color: #DB1E60; font-size: .8em;">ASP.NET Web Forms + C# + jQuery + SQL Server</span> - my supervisors asked to me to mount a development team and to write the development process for the company, so I did it based on agile methodologies like <span style="color: #DB1E60; font-size: .8em;">Scrum</span>.`
+      When the first version was delivered - which I wrote in <span class="highlight smaller-font">ASP.NET Web Forms + C# + jQuery + SQL Server</span> - my supervisors asked to me to mount a development team and to write the development process for the company, so I did it based on agile methodologies like <span class="highlight smaller-font">Scrum</span>.`
     },
     {
       title: "Developer",
@@ -56,8 +56,8 @@ export const EXPERIENCES: Experience[] = [
       companyLogo: "assets/img/companies/apply.jpg",
       startDate: "Aug 2009",
       endDate: "Sep 2011",
-      description: `Doing part of a development team for a quotation system written in <span style="color: #DB1E60; font-size: .8em;">ASP.NET Web Forms, SQL Server, jQuery and Report Viewer</span>, I was able, after a while, to become the leader of this team due to my interventions in the project.
+      description: `Doing part of a development team for a quotation system written in <span class="highlight smaller-font">ASP.NET Web Forms, SQL Server, jQuery and Report Viewer</span>, I was able, after a while, to become the leader of this team due to my interventions in the project.
 
-      I also was responsible, as an outsourced professional, to create an architecture in <span style="color: #DB1E60; font-size: .8em;">Zend (PHP)</span> for a CRM system to a company called Nagem.`
+      I also was responsible, as an outsourced professional, to create an architecture in <span class="highlight smaller-font">Zend (PHP)</span> for a CRM system to a company called Nagem.`
     }
   ];
