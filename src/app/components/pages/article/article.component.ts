50d8faead7f1fb208a89440bd500b4e17d5807c4
Luiz Galiza <lfdgaliza@gmail.com>
2018-05-28 22:37:58 +1000

    articles with img ok

--- src/app/components/pages/article/article.component.ts
+++ src/app/components/pages/article/article.component.ts
@@ -3,6 +3,8 @@ import { GitHubService } from '../../../services/git-hub.service';
 import { ArticlesMenuService } from '../../../services/articles-menu.service';
 import { BaseMenuItem } from '../../../models/base/base-menu-item.model';
 import { ArticleMenuItem } from '../../../models/article-menu-item.model';
+import { MarkdownService } from 'ngx-markdown';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'gl-article',
@@ -17,16 +19,35 @@ export class ArticleComponent implements OnInit {
 
   constructor(
     private _gitHubService: GitHubService,
-    private _articlesMenuService: ArticlesMenuService) { }
+    private _articlesMenuService: ArticlesMenuService,
+    private _markdownService: MarkdownService) { }
 
   ngOnInit() {
+
     this._articlesMenuService.getAll().subscribe(menuItems => {
       this.menuItems = menuItems;
     });
   }
 
   showArticle(item: ArticleMenuItem) {
+
     console.log(item);
+
+
+
+
+    this._markdownService.renderer.image = (href: string, title: string, text: string) => {
+      console.log(href, title, text);
+
+      if (href.startsWith("/")) {
+        let indexFirstBar = item.Path.indexOf("/");
+        let repoName = item.Path.substring(0, indexFirstBar);
+        href = `${environment.githubBaseUrl}/${repoName}/master${href}`;
+      }
+
+      return `<img src="${href}" class="img-fluid" />`;
+    }
+
     this._gitHubService.getArticle(item).subscribe(result => {
       this.article = result;
     });
