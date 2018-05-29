44ab8a88423456cd75dde94e4f13f096aae67eb3
Luiz Galiza <lfdgaliza@gmail.com>
2018-05-29 23:45:07 +1000

    environment file updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   articlesMenuApiUrl: "https://blmvayqswe.execute-api.us-east-1.amazonaws.com/Production/articles/menu",
-  githubBaseUrl: "https://cdn.rawgit.com/lfdgaliza"
+  githubRawBaseUrl: "https://cdn.rawgit.com/lfdgaliza",
+  githubBaseUrl: "https://github.com/lfdgaliza"
 };
